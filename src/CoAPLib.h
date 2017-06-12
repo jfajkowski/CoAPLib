e1114606c72b00d2dd7944268783c35c9b95bdd8
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2017-06-12 17:42:47 +0200

    Added includes.

--- src/CoAPLib.h
+++ src/CoAPLib.h
@@ -2,11 +2,11 @@
 #define CoAPLib_h
 
 #include "CoAPLib/Array.hpp"
-#include "CoAPLib/CoAPMessage.h"
-#include "CoAPLib/CoAPOption.h"
 #include "CoAPLib/CoAPConstants.h"
-#include "CoAPLib/Environment.h"
-#include "RadioLib/RadioMessage.hpp"
 #include "CoAPLib/CoAPHandler.h"
+#include "CoAPLib/CoAPMessage.h"
+#include "CoAPLib/CoAPMessageListener.h"
+#include "CoAPLib/CoAPOption.h"
+#include "CoAPLib/Environment.h"
 
 #endif //CoAPLib_h
