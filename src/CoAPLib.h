61e6d0b2f9248dde67fcf103d68797a3d2106434
Jakub Fajkowski <jakub.fajkowski@gmail.com>
2017-06-12 17:41:16 +0200

    Renamed. :3

--- src/CoAPLib.h
+++ src/CoAPLib.h
@@ -4,7 +4,7 @@
 #include "CoAPLib/Array.hpp"
 #include "CoAPLib/CoAPMessage.h"
 #include "CoAPLib/CoAPOption.h"
-#include "CoAPLib/Constants.h"
+#include "CoAPLib/CoAPConstants.h"
 #include "CoAPLib/Environment.h"
 #include "RadioLib/RadioMessage.hpp"
 #include "CoAPLib/CoAPHandler.h"
